398a1a004718acc0b631b48101a4610cfe188086
Jules Fouchy <jules.fouchy@ntymail.com>
2023-08-12 22:27:08 +0200

    🐛 Fix memory leak on MacOS

--- src/webcam_info.mm
+++ src/webcam_info.mm
@@ -7,30 +7,32 @@
 namespace webcam_info {
 
 auto grab_all_webcams_infos_impl() -> std::vector<Info> {
-  std::vector<Info> list_webcams_infos{};
+    std::vector<Info> list_webcams_infos{};
 
-  AVCaptureDeviceDiscoverySession *discoverySession =
-      [AVCaptureDeviceDiscoverySession
-          discoverySessionWithDeviceTypes:@[
-            AVCaptureDeviceTypeBuiltInWideAngleCamera
-          ]
-                                mediaType:AVMediaTypeVideo
-                                 position:AVCaptureDevicePositionUnspecified];
+    AVCaptureDeviceDiscoverySession *discoverySession =
+        [AVCaptureDeviceDiscoverySession
+            discoverySessionWithDeviceTypes:@[
+                AVCaptureDeviceTypeBuiltInWideAngleCamera
+            ]
+            mediaType:AVMediaTypeVideo
+            position:AVCaptureDevicePositionUnspecified];
 
-  NSArray *devices = discoverySession.devices;
+    NSArray *devices = discoverySession.devices;
 
-  for (AVCaptureDevice *device in devices) {
-    std::string deviceName = [device.localizedName UTF8String];
-    std::vector<webcam_info::Resolution> list_resolution{};
+    for (AVCaptureDevice *device in devices) {
+        @autoreleasepool {
+            std::string deviceName = [device.localizedName UTF8String];
+            std::vector<webcam_info::Resolution> list_resolution{};
 
-    for (AVCaptureDeviceFormat *format in device.formats) {
-      CMVideoDimensions dimensions =
-          CMVideoFormatDescriptionGetDimensions(format.formatDescription);
-      list_resolution.push_back({dimensions.width, dimensions.height});
+            for (AVCaptureDeviceFormat *format in device.formats) {
+                CMVideoDimensions dimensions =
+                    CMVideoFormatDescriptionGetDimensions(format.formatDescription);
+                list_resolution.push_back({dimensions.width, dimensions.height});
+            }
+            list_webcams_infos.push_back({deviceName, list_resolution});
+        }
     }
-    list_webcams_infos.push_back({deviceName, list_resolution});
-  }
-  return list_webcams_infos;
+    return list_webcams_infos;
 }
 
 } // namespace webcam_info
