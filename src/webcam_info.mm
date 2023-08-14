a3b3ae556f187ea07a13c22789b3b6ee2ca71659
Jules Fouchy <jules.fouchy@ntymail.com>
2023-08-14 23:48:06 +0200

    🐛 Mac: fix some webcams not beeing found + maybe fix memory leak

--- src/webcam_info.mm
+++ src/webcam_info.mm
@@ -9,31 +9,36 @@ namespace webcam_info {
 auto grab_all_webcams_infos_impl() -> std::vector<Info> {
     std::vector<Info> list_webcams_infos{};
 
-    AVCaptureDeviceDiscoverySession *discoverySession =
-        [AVCaptureDeviceDiscoverySession
-            discoverySessionWithDeviceTypes:@[
-                AVCaptureDeviceTypeBuiltInWideAngleCamera
-            ]
-            mediaType:AVMediaTypeVideo
-            position:AVCaptureDevicePositionUnspecified];
+    @autoreleasepool
+    {
+        AVCaptureDeviceDiscoverySession *discoverySession =
+            [AVCaptureDeviceDiscoverySession
+                discoverySessionWithDeviceTypes:@[
+                    AVCaptureDeviceTypeBuiltInWideAngleCamera,
+                    AVCaptureDeviceTypeBuiltInUltraWideCamera,
+                    AVCaptureDeviceTypeBuiltInTelephotoCamera,
+                    AVCaptureDeviceTypeBuiltInDualCamera,
+                    AVCaptureDeviceTypeBuiltInDualWideCamera,
+                    AVCaptureDeviceTypeBuiltInTripleCamera,
+                    AVCaptureDeviceTypeContinuityCamera
+                ]
+                mediaType:AVMediaTypeVideo
+                position:AVCaptureDevicePositionUnspecified];
 
-    if (!discoverySession) {
-        return list_webcams_infos;
-    }
+        if (!discoverySession)
+            return list_webcams_infos;
 
-    NSArray *devices = discoverySession.devices;
+        NSArray *devices = discoverySession.devices;
 
-    for (AVCaptureDevice *device in devices) {
-        @autoreleasepool {
-            std::string deviceName = [[device localizedName] UTF8String];
-            std::vector<webcam_info::Resolution> list_resolution{};
+        for (AVCaptureDevice *device in devices) {
+                std::string deviceName = [[device localizedName] UTF8String];
+                std::vector<webcam_info::Resolution> list_resolution{};
 
-            for (AVCaptureDeviceFormat *format in device.formats) {
-                CMVideoDimensions dimensions =
-                    CMVideoFormatDescriptionGetDimensions(format.formatDescription);
-                list_resolution.push_back({dimensions.width, dimensions.height});
-            }
-            list_webcams_infos.push_back({deviceName, list_resolution});
+                for (AVCaptureDeviceFormat *format in device.formats) {
+                    CMVideoDimensions dimensions = CMVideoFormatDescriptionGetDimensions(format.formatDescription);
+                    list_resolution.push_back({dimensions.width, dimensions.height});
+                }
+                list_webcams_infos.push_back({deviceName, list_resolution});
         }
     }
     return list_webcams_infos;
