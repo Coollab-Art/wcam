572309d59508e715bc9e6412656e53e421b3838e
Jules Fouchy <jules.fouchy@ntymail.com>
2024-08-01 18:36:44 +0200

    🐛 [Tests] Fix creation of texture

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,7 @@
 #include <quick_imgui/quick_imgui.hpp>
 #include <webcam_info/webcam_info.hpp>
-#include "imgui.h"
 #include "glad/glad.h"
+#include "imgui.h"
 
 auto make_texture() -> GLuint
 {
@@ -20,8 +20,10 @@ auto make_texture() -> GLuint
 auto main() -> int
 {
     std::unique_ptr<webcam::Capture> capture;
-    GLuint                         texture_id;    // NOLINT(*init-variables)
-    quick_imgui::loop("webcam_info tests", [&]() { // Open a window and run all the ImGui-related code
+    GLuint                           texture_id{0}; // NOLINT(*init-variables)
+    quick_imgui::loop("webcam_info tests", [&]() {  // Open a window and run all the ImGui-related code
+        if (texture_id == 0)
+            texture_id = make_texture();
         ImGui::Begin("webcam_info tests");
 
         auto const webcam_infos = webcam::grab_all_infos();
@@ -29,9 +31,9 @@ auto main() -> int
         {
             if (ImGui::CollapsingHeader(info.name.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
             {
-                for (size_t i {0}; i < info.available_resolutions.size(); i++)
+                for (size_t i{0}; i < info.available_resolutions.size(); i++)
                 {
-                    auto const &resolution {info.available_resolutions[i]};
+                    auto const& resolution{info.available_resolutions[i]};
                     ImGui::Text("width : %d / height : %d", resolution.width(), resolution.height());
                     ImGui::PushID(i);
                     if (ImGui::Button("Open webcam"))
@@ -45,10 +47,11 @@ auto main() -> int
         if (capture != nullptr)
         {
             auto const image = capture->image();
-            if (image.has_value()){
-            glBindTexture(GL_TEXTURE_2D, texture_id);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width(), image->height(), 0, GL_RGB, GL_UNSIGNED_BYTE, image->data());
-            ImGui::Image(static_cast<ImTextureID>(reinterpret_cast<void*>(static_cast<uint64_t>(texture_id))), ImVec2{850.f * static_cast<float>(image->width()) / static_cast<float>(image->height()), 850.f}); // NOLINT(performance-no-int-to-ptr, *reinterpret-cast)
+            if (image.has_value())
+            {
+                glBindTexture(GL_TEXTURE_2D, texture_id);
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width(), image->height(), 0, GL_BGR, GL_UNSIGNED_BYTE, image->data());
+                ImGui::Image(static_cast<ImTextureID>(reinterpret_cast<void*>(static_cast<uint64_t>(texture_id))), ImVec2{850.f * static_cast<float>(image->width()) / static_cast<float>(image->height()), 850.f}, ImVec2(0., 1.), ImVec2(1., 0.)); // NOLINT(performance-no-int-to-ptr, *reinterpret-cast)
             }
         }
         ImGui::End();
