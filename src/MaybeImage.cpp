8af4ba490854fb5516fbe920cc07a20eb925c47d
Jules Fouchy <jules.fouchy@ntymail.com>
2025-07-25 11:41:14 +0200

    🤏 Improve error messages

--- src/MaybeImage.cpp
+++ src/MaybeImage.cpp
@@ -9,10 +9,10 @@ auto to_string(CaptureError const& err) -> std::string
     return std::visit(
         overloaded{
             [](Error_WebcamAlreadyUsedInAnotherApplication const&) {
-                return "Another application is already using the camera. You need to close that other application in order for us to be able to access the camera."s;
+                return "Another application is already using that camera. You need to close that other application in order for us to be able to access the camera."s;
             },
             [](Error_WebcamUnplugged const&) {
-                return "The camera has been unplugged. You need to re-plug it in."s;
+                return "That camera has been unplugged. You need to plug it back in."s;
             },
             [](Error_Unknown const& err) {
                 return "Unexpected error: "s + err.message;
